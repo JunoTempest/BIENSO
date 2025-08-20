52f1e6f75dfd6b11d6ffb851b1308faa8005bbe7
JunoTempest <nguyentrhieu11a7@gmail.com>
2025-08-21 03:04:10 +0700

    Delete BIENSO directory

--- BIENSO/BTL/Controller/ctl.py
+++
@@ -1,3 +0,0 @@
-class A_ctl:
-    def __init__(self):
-        pass
--- BIENSO/BTL/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from View.mainview import Mainview
-from Controller.ctl import A_ctl
-
-if __name__ == "__main__":
-    window = tk.Tk()
-    controller = A_ctl()
-    app = Mainview(window, controller)
-    window.mainloop()
